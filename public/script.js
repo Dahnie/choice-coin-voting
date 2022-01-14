330e1a13d70f12a557c5e1d5da141433b46d3eea
Dan Adeneye <adeneyedaniel007@gmail.com>
2022-01-14 13:26:30 +0100

    recent changes

--- public/script.js
+++ public/script.js
@@ -15,6 +15,7 @@ const voteNo = document.querySelector("#no");
 const submitVoteBtn = document.querySelector("#submit-vote");
 let voteOptions = document.getElementsByName("vote-output")
 let transactionID = "";
+let voterAddress = "";
 
 
 
@@ -25,6 +26,7 @@ const getChoiceCoinValue = function (input) {
         console.log("Choice Value", choiceCoinAmount);
     })
 }
+
 //Function that gets Issue Number and assign
 const getIssueNumber = function (input) {
     input.addEventListener("change", (e) => {
@@ -96,66 +98,90 @@ const performTransaction = async (receiverAddress, senderAddress, choiceAmount) 
             console.error(e);
         });
 
-    // Get accountss
+    // Generate accounts from TestNet app
     AlgoSigner.accounts({
         ledger: 'TestNet'
     })
-        .then((d) => {
-            accounts = d;
-            console.log("accounts", accounts);
+        .then((data) => {
+            accounts = data;
+            voterAddress = accounts[2]["address"]
+            console.log("accounts-------", accounts);
         })
         .catch((e) => {
             console.error(e);
         });
 
+    // Generate transaction paramenters
     algodClient.getTransactionParams().do()
-        .then((d) => {
-            txParamsJS = d;
+        .then((data) => {
+            txParamsJS = data;
             console.log("txParamsJS............", txParamsJS);
+            // alert(txParamsJS)
         })
-        .catch((e) => {
-            console.error(e);
+        .catch((err) => {
+            console.error(err);
         });
+    AlgoSigner.algod({
+        ledger: "TestNet",
+        path: "/v2/transactions/params"
+    })
+        .then((data) => {
+            txParamsJS = data;
+            console.log("txParams-------------", txParamsJS);
+            alert(txParamsJS)
 
-    const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
-        from: senderAddress,
-        to: receiverAddress,
-        amount: +choiceAmount,
-        suggestedParams: {
-            "flatFee": false,
-            "fee": 0,
-            "firstRound": 18674453,
-            "lastRound": 18675453,
-            "genesisID": "testnet-v1.0",
-            "genesisHash": "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI="
-        }
-    });
+            // Agosdk method that executes payment to addresses.              
+            const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+                // TODO Final todo is to generate senderAddress from the API response from the account-generating stuff, not manually inputing it.
+                from: senderAddress,
+                to: receiverAddress,
+                amount: +choiceAmount * 1_000_000,
+                suggestedParams:
+                {
+                    "flatFee": false,
+                    "fee": 0,
+                    "firstRound": (Number(txParamsJS["last-round"])),
+                    "lastRound": Number(txParamsJS["last-round"]) + 1000,
+                    // 18684498--18685498
+                    "genesisID": "testnet-v1.0",
+                    "genesisHash": "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI="
+                }
+            });
 
-    // Use the AlgoSigner encoding library to make the transactions base64
-    let txn_b64 = AlgoSigner.encoding.msgpackToBase64(txn.toByte());
 
-    AlgoSigner.signTxn([{ txn: txn_b64 }])
-        .then((d) => {
-            signedTxs = d;
-            console.log("Signed Txn----", signedTxs);
-            transactionID = signedTxs[0].txID
-            AlgoSigner.send({
-                ledger: 'TestNet',
-                tx: signedTxs[0].blob
-            })
-                .then((d) => {
-                    tx = d;
-                    alert(`transaction complete with transaction id: ${transactionID}`)
+
+
+            // Use the AlgoSigner encoding library to make the transactions base64
+            let txn_b64 = AlgoSigner.encoding.msgpackToBase64(txn.toByte());
+
+            // Sign the transaction and send the coin appropriately and also display the Transaction ID.
+            AlgoSigner.signTxn([{ txn: txn_b64 }])
+                .then((data) => {
+                    signedTxs = data;
+                    console.log("Signed Txn----", signedTxs);
+                    transactionID = signedTxs[0].txID
+                    AlgoSigner.send({
+                        ledger: 'TestNet',
+                        tx: signedTxs[0].blob
+                    })
+                        .then((data) => {
+                            tx = data;
+                            alert(`transaction complete with transaction id: ${transactionID}`)
+                        })
+                        .catch((err) => {
+                            console.error(err.message);
+                            alert(`Transaction with ID ${transactionID} encountered error: ${err.message}`,)
+
+                        });
                 })
                 .catch((err) => {
                     console.error(err.message);
-                    alert(`Transaction with ID ${transactionID} encountered error: ${err.message}`,)
-
                 });
         })
-        .catch((e) => {
-            console.error(e);
-        });
+        .catch((err) => {
+            console.log(err.message);
+        })
+
 }
 
 
